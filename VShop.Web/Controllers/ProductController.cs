9dd85dfa7312f61774010afbe835cacf760702d1
chmov <chmov@PC-CARLOSHMV>
2026-04-27 13:56:00 -0300

    feat: Implement ProductController

---
+++ VShop.Web/Controllers/ProductController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using VShop.Web.Models;
+using VShop.Web.Services.Contracts;
+
+namespace VShop.Web.Controllers
+{
+    public class ProductController : Controller
+    {
+        private readonly IProductService _productService;
+        private readonly ICategoryService _categoryService;
+        public ProductController(IProductService productService, ICategoryService categoryService)
+        {
+            _productService = productService;
+            _categoryService = categoryService;
+        }
+
+
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<ProductViewModel>>> Index()
+        {
+
+            var result = await _productService.GetAllProducts();
+
+            if (result is null)
+                return View("Error");
+
+            return View(result);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> CreateProduct()
+        {
+            ViewBag.CategoryId = new SelectList(await _categoryService.GetAllCategories(), "CategoryId", "Name");
+            return View();
+        }
+    }
+}
